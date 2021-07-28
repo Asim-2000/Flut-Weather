469bf06357f5f65d036c97de4f470e697593955d
Muhammad Asim Khaskheli <mkhaskheli.bese18seecs@seecs.edu.pk>
2021-07-28 14:47:34 +0500

    📦 NEW: form validation added in login screen

--- lib/screens/login-screen.dart
+++ lib/screens/login-screen.dart
@@ -10,54 +10,75 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  String Name = "";
+  String name = "";
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/images/login.png',
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Hello World $Name",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 32.0, vertical: 16.0),
-                child: Column(children: [
-                  TextFormField(
-                    onChanged: (value) {
-                      setState(() {
-                        Name = value;
-                      }); // setState calls build method again i.e it re renders UI
-                    },
-                    decoration: InputDecoration(
-                        hintText: "Enter UserName", labelText: "UserName"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Enter Password", labelText: "Password"),
-                  ),
-                  SizedBox(height: 40.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, HomeScreen.routeName);
-                    },
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(100, 40)),
-                  )
-                ]),
-              )
-            ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/login.png',
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Hello World $name",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0, vertical: 16.0),
+                  child: Column(children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return ("Username can't be empty");
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          name = value;
+                        }); // setState calls build method again i.e it re renders UI
+                      },
+                      decoration: InputDecoration(
+                          hintText: "Enter UserName", labelText: "UserName"),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return ("Password can't be empty");
+                        }
+                        if (value.length < 6) {
+                          return ("Password Length should be atleast 6");
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter Password", labelText: "Password"),
+                    ),
+                    SizedBox(height: 40.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          Navigator.pushNamed(context, HomeScreen.routeName);
+                        }
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+                    )
+                  ]),
+                )
+              ],
+            ),
           ),
         ));
   }
